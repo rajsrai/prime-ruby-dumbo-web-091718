1557bea3b0f16410771154787af6a766d814fbe0
Rajvir Rai <rsr1992@gmail.com>
2018-09-09 02:46:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
-# Add  code here!
+def prime?(x)
+#   if x % 2 != 0  || x = 2
+#     return true
+#   else x <= 0 && x % 2 == 0
+#   return false
+# end
+# end
+
+
+if x == 2
+  true 
+end
+  if x == -1
+     false
+elsif
+  x % 2 == 0
+  false
+else
+  true
+end
+end
+
+num.each do |x| 
+  
