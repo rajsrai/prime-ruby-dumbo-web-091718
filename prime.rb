4826eb5032aacabe0dfc1b156e62429ed733e3d0
Rajvir Rai <rsr1992@gmail.com>
2018-09-09 02:49:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime?(x)
+#   if x % 2 != 0  || x = 2
+#     return true
+#   else x <= 0 && x % 2 == 0
+#   return false
+# end
+# end
+
+if x <= 1 
+  return false 
+end
+i = 2 
+ while i < x 
+  if x % i == 0 
+    return false
+  end
+  i+=1 
+end
+true
+end
